8653c222cc7298ac480b746a8fe2b49f180eea8d
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2023-10-03 01:00:51 +0500

    weather end

--- js/app.js
+++ js/app.js
@@ -4,3 +4,39 @@ const details = document.getElementById('details')
 const weatherIcon = document.getElementById('weather-icon')
 const overlay = document.getElementById('overlay')
 
+const updetaUI = (city)=>{
+    details.innerHTML = `
+    <h5 class="mb-3">${city.name}, ${city.sys.country}</h5>
+    <p class="mb-3">${city.weather[0].main}</p>
+    <div class="display-4 mb-3">
+      <span>${Math.round(city.main.temp)}</span>
+      <span>&deg;C</span>
+    </div>
+    `    
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+    
+
+    weatherIcon.src = `https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`;
+    weatherIcon.alt = `${city.weather[0].icon} icons`
+    console.log(city.main);
+}
+
+
+
+
+// get wather
+const getWeather = async (city)=>{
+    let data = await getData(city);
+    return data;
+}
+// change form
+changeLocation.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let cityName = changeLocation.city.value.trim();   
+    console.log(cityName);
+    changeLocation.reset();
+    getWeather(cityName).then(data => updetaUI(data));
+})
+
--- js/forecast.js
+++ js/forecast.js
@@ -1,5 +1,12 @@
 const KEY = '96b947a45d33d7dc1c49af3203966408'
 
-const getData = (city) =>{
+const getData = async (city) => {
     const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+    const rec = `?q=${city}&units=metric&appid=${KEY}`
+
+    const req = await fetch(baseURL+rec)
+    const data = await req.json();
+
+    return data;
 }
+
